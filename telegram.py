238010ec3687d74e06d55d672e665b2780201586
alessuy <apalermo.uy@gmail.com>
2019-09-28 23:28:46 -0300

    update telegram

---
+++ telegram.py
@@ -0,0 +1,12 @@
+import time
+import datetime
+import telepot
+from telepot.loop import MessageLoop
+
+
+bot = telepot.Bot('apikeytelegram')
+foto = open('/home/pi/timbre/timbre.jpg','rb')
+bot.sendPhoto(-342267662,foto)
+bot.sendMessage(-342267662, 'Riiing 1117')
+
+
