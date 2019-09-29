2afe5c32d4c6f2b4ca815804ab7b77d413aef004
alessuy <32991508+alessuy@users.noreply.github.com>
2019-09-28 23:07:42 -0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -4,7 +4,7 @@ import telepot
 from telepot.loop import MessageLoop
 
 
-bot = telepot.Bot('779238274:AAE5ZLrWaVaBzevzCouvag2osqzT7DTmSiM')
+bot = telepot.Bot('hajHAJajjajAaHSAJSASH')
 foto = open('/home/pi/timbre/timbre.jpg','rb')
 bot.sendPhoto(-342267662,foto)
 bot.sendMessage(-342267662, 'Riiing 1117')
